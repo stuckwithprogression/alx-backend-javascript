77a74b86199464ea804f62bdb688ad99ad3382f7
stuckwithprogression <chukwunwemichael@gmail.com>
2024-06-19 02:02:58 +0100

    fix errors

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -2,16 +2,13 @@ const express = require('express');
 
 const app = express();
 const PORT = 1245;
-const HOST = 'localhost';
-const URL = `http://${HOST}:${PORT}`;
-
 
 app.get('/', (_, res) => {
   res.send('Hello Holberton School!');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on ${URL}`);
+  console.log(`Server listening on PORT ${PORT}`);
 });
 
 module.exports = app;
